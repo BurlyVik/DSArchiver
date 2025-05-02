5234304146fbc237a2a4f074e3e252880fc94e22
BurlyVik <161164589+BurlyVik@users.noreply.github.com>
2025-05-02 19:49:52 -0400

    Update dsarchiver.ps1

--- dsarchiver.ps1
+++ dsarchiver.ps1
@@ -1,6 +1,20 @@
 
 # Change [USER] to user profile name
 # Change [SaveID] to game Save ID (numeric)
+function Test-PathInUse {
+    param($Path)
+    try {
+        # Grabs a random file in the directory, ensures its not in use by ds
+        $tempFile = [System.IO.Path]::Combine($Path, [System.IO.Path]::GetRandomFileName())
+        $stream = [System.IO.File]::Create($tempFile)
+        $stream.Close()
+        Remove-Item $tempFile -Force
+        return $false
+    } catch {
+        return $true
+    }
+}
+
 function DSArchiver
 {
     Clear-Host
